07852522348fdf72bdffae4f610982f89fc39eba
Julian Römer <julian_romer@orange.fr>
2021-03-02 16:17:28 +0100

    finished color check

--- color_check/controllers/get_color_code.py
+++ color_check/controllers/get_color_code.py
@@ -14,10 +14,13 @@ with open('color_check/data/css-color-names.json') as f:
 print(color_code)
 
 
-def get_color_code(color_name):
+def get_color_code(user_submitted_string):
     # this is where you should add your logic to check the color.
     # Open the file at data/css-color-names.json, and return the hex code
     # The file can be considered as JSON format, or as a Python dictionary.
+    for key in color_code:
+        if key == user_submitted_string:
+            print(color_code[user_submitted_string])
 
     hex_code = '#0000ff'
 
