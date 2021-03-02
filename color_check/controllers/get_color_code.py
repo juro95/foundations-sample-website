625d7964563eea00e29b6a5d3df3017907cfe718
Julian Römer <julian_romer@orange.fr>
2021-03-02 15:36:46 +0100

    changed

--- color_check/controllers/get_color_code.py
+++ color_check/controllers/get_color_code.py
@@ -5,6 +5,14 @@
 # raise and handle an error that helps both you as a developer,
 # for example by logging the request and error, and the user,
 # letting them know that their color doesn't exist.
+import json
+
+
+with open('color_check/data/css-color-names.json') as f:
+    color_code = json.load(f)
+
+print(color_code)
+
 
 def get_color_code(color_name):
     # this is where you should add your logic to check the color.
