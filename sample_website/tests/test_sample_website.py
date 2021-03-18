d00b0dddfe26a4562ce59634f0e5013a444b1e53
Julian Römer <julian_romer@orange.fr>
2021-03-18 14:10:42 +0100

    changed main.py

--- sample_website/tests/test_sample_website.py
+++ sample_website/tests/test_sample_website.py
@@ -2,8 +2,4 @@ from sample_website.website import hello_world
 
 
 def test_sample_website():
-<<<<<<< HEAD
-    assert hello_world() == '<h1>Hello, Class!</h1>'
-=======
     assert hello_world() == '<h1>Hello, Julian!</h1>'
->>>>>>> e6a78af (change greeting)
