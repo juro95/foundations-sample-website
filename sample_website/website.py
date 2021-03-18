1904d8bdad6ccd44b8be9f823e6b609f7bf1ffe6
Julian Römer <julian_romer@orange.fr>
2021-03-18 12:36:37 +0100

    change greeting

--- sample_website/website.py
+++ sample_website/website.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+<<<<<<< HEAD
     return '<h1>Hello, Class!</h1>'
+=======
+    return '<h1>Hello, Julian!</h1>'
+>>>>>>> e6a78af (change greeting)
 
 
 if __name__ == "__main__":
